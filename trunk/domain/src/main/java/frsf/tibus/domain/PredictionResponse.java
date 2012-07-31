c57ba0283a64bcf728ad84db073d2ff01a8f9893
digasimi@gmail.com <digasimi@gmail.com@1eb605dd-4156-0cf0-e7f9-35fc125b5a16>
2012-07-31 22:18:26 +0000

    Agregado Schema XML PredictionRequest y Prediction Response

--- trunk/domain/src/main/java/frsf/tibus/domain/PredictionResponse.java
+++ trunk/domain/src/main/java/frsf/tibus/domain/PredictionResponse.java
@@ -1,25 +1,203 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.5-2 
+// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: PM.07.31 a las 07:13:02 PM ART 
+//
+
 
 package frsf.tibus.domain;
 
-import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.datatype.XMLGregorianCalendar;
+
 
 /**
- *
- * @author dani
+ * <p>Clase Java para anonymous complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="prediction" maxOccurs="unbounded" minOccurs="0">
+ *           &lt;complexType>
+ *             &lt;complexContent>
+ *               &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *                 &lt;sequence>
+ *                   &lt;element name="colectivo" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *                   &lt;element name="tiempo" type="{http://www.w3.org/2001/XMLSchema}decimal"/>
+ *                 &lt;/sequence>
+ *               &lt;/restriction>
+ *             &lt;/complexContent>
+ *           &lt;/complexType>
+ *         &lt;/element>
+ *         &lt;element name="timestamp" type="{http://www.w3.org/2001/XMLSchema}dateTime"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
  */
-public class PredictionResponse  implements Serializable {
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "prediction",
+    "timestamp"
+})
+@XmlRootElement(name = "prediction-response")
+public class PredictionResponse {
 
-    private final String tiempoPrediccion;
+    protected List<PredictionResponse.Prediction> prediction;
+    @XmlElement(required = true)
+    @XmlSchemaType(name = "dateTime")
+    protected XMLGregorianCalendar timestamp;
 
-    public PredictionResponse(String tiempoPrediccion) {
-        this.tiempoPrediccion = tiempoPrediccion;
+    /**
+     * Gets the value of the prediction property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the prediction property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getPrediction().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link PredictionResponse.Prediction }
+     * 
+     * 
+     */
+    public List<PredictionResponse.Prediction> getPrediction() {
+        if (prediction == null) {
+            prediction = new ArrayList<PredictionResponse.Prediction>();
+        }
+        return this.prediction;
     }
 
-    public String getTiempoPrediccion() {
-        return tiempoPrediccion;
+    /**
+     * Obtiene el valor de la propiedad timestamp.
+     * 
+     * @return
+     *     possible object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public XMLGregorianCalendar getTimestamp() {
+        return timestamp;
     }
+
+    /**
+     * Define el valor de la propiedad timestamp.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public void setTimestamp(XMLGregorianCalendar value) {
+        this.timestamp = value;
+    }
+
+
+    /**
+     * <p>Clase Java para anonymous complex type.
+     * 
+     * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+     * 
+     * <pre>
+     * &lt;complexType>
+     *   &lt;complexContent>
+     *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+     *       &lt;sequence>
+     *         &lt;element name="colectivo" type="{http://www.w3.org/2001/XMLSchema}string"/>
+     *         &lt;element name="tiempo" type="{http://www.w3.org/2001/XMLSchema}decimal"/>
+     *       &lt;/sequence>
+     *     &lt;/restriction>
+     *   &lt;/complexContent>
+     * &lt;/complexType>
+     * </pre>
+     * 
+     * 
+     */
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @XmlType(name = "", propOrder = {
+        "colectivo",
+        "tiempo"
+    })
+    public static class Prediction {
+
+        @XmlElement(required = true)
+        protected String colectivo;
+        @XmlElement(required = true)
+        protected BigDecimal tiempo;
+
+        /**
+         * Obtiene el valor de la propiedad colectivo.
+         * 
+         * @return
+         *     possible object is
+         *     {@link String }
+         *     
+         */
+        public String getColectivo() {
+            return colectivo;
+        }
+
+        /**
+         * Define el valor de la propiedad colectivo.
+         * 
+         * @param value
+         *     allowed object is
+         *     {@link String }
+         *     
+         */
+        public void setColectivo(String value) {
+            this.colectivo = value;
+        }
+
+        /**
+         * Obtiene el valor de la propiedad tiempo.
+         * 
+         * @return
+         *     possible object is
+         *     {@link BigDecimal }
+         *     
+         */
+        public BigDecimal getTiempo() {
+            return tiempo;
+        }
+
+        /**
+         * Define el valor de la propiedad tiempo.
+         * 
+         * @param value
+         *     allowed object is
+         *     {@link BigDecimal }
+         *     
+         */
+        public void setTiempo(BigDecimal value) {
+            this.tiempo = value;
+        }
+
+    }
+
 }
