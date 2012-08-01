0cc6721ff6260ea6870694967a51df9d41f7606e
digasimi@gmail.com <digasimi@gmail.com@1eb605dd-4156-0cf0-e7f9-35fc125b5a16>
2012-08-01 20:29:37 +0000

    Eliminado mapa de administracion de lineas y unidades. Solucionado el problema en la prediccion. Eliminado dato coordenada del modelo de django. Refactorizado de templates.

--- webTibus/timetobus/settings.py
+++ webTibus/timetobus/settings.py
@@ -116,7 +116,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/diego/Documentos/timetobus/mytemplates",
+    "/home/diego/Documentos/Workspace/timetobus/mytemplates",
     "/home/dani/tibus/timetobus/mytemplates", 
 )
 
