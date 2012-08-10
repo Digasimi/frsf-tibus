89feb91b2d03742382ac47e47a5c4884dd4d147d
digasimi@gmail.com <digasimi@gmail.com@1eb605dd-4156-0cf0-e7f9-35fc125b5a16>
2012-08-10 00:45:54 +0000

    webtibus: Corregido muestra de tiempos de prediccion. Agregado TimeStamp de prediccion.
    bug: Mal lectura de latitud/longitud del colectivo

--- trunk/webTibus/tibus/models.py
+++ trunk/webTibus/tibus/models.py
@@ -57,61 +57,60 @@ class Recorrido(models.Model):
 
 
 class Parada(models.Model):
-       idparada = models.AutoField(primary_key=True) #agregado para el cambio de tablespace
-       orden = models.IntegerField()
-       objects = models.GeoManager()
-       latitud = models.FloatField()
-       longitud = models.FloatField()
-       #coordenadas = models.PointField(srid=4326)
-       linea = models.ForeignKey(Recorrido)
-       calle1 = models.CharField(verbose_name="calle1", max_length=100)
-       calle2 = models.CharField(verbose_name="calle2", max_length=100)
-       proximaparada = models.IntegerField(null=True)
+    idparada = models.AutoField(primary_key=True) #agregado para el cambio de tablespace
+    orden = models.IntegerField()
+    objects = models.GeoManager()
+    latitud = models.FloatField()
+    longitud = models.FloatField()
+    linea = models.ForeignKey(Recorrido)
+    calle1 = models.CharField(verbose_name="calle1", max_length=100)
+    calle2 = models.CharField(verbose_name="calle2", max_length=100)
+    proximaparada = models.IntegerField(null=True)
   
-       class Meta:
-          db_table = 'parada'
+    class Meta:
+        db_table = 'parada'
         
-       def _unicode_(self):
-          return self.orden
+    def _unicode_(self):
+        return self.orden
           
-       def ordenParada(self):
-          return self.orden
-
-       def latitudParada(self):
-          return self.latitud
-         
-       def longitudParada(self):
-          return self.longitud
-          
-       def aumentarOrden(self):
-           self.orden = self.orden + 1
-           return True
-           
-       def disminuirOrden(self):
-           self.orden = self.orden - 1
-           return True
+    def ordenParada(self):
+    	return self.orden
+    
+    def latitudParada(self):
+		return self.latitud
+      
+    def longitudParada(self):
+		return self.longitud
+       
+    def aumentarOrden(self):
+        self.orden = self.orden + 1
+        return True
+        
+    def disminuirOrden(self):
+        self.orden = self.orden - 1
+        return True
       
 class Unidad(models.Model):
-      idunidad = models.AutoField(primary_key=True) #agregado para el cambio de tablespace
-      linea = models.ForeignKey(Recorrido)
-      aptoMovilidadReducida = models.BooleanField()
-      id_unidad_linea = models.IntegerField()
+    idunidad = models.AutoField(primary_key=True) #agregado para el cambio de tablespace
+    linea = models.ForeignKey(Recorrido)
+    aptoMovilidadReducida = models.BooleanField()
+    id_unidad_linea = models.IntegerField()
       
-      class Meta:
-          db_table = 'unidad'
+    class Meta:
+        db_table = 'unidad'
     
 class PosicionActual(models.Model):
-      latitud = models.FloatField()
-      longitud = models.FloatField()
-      paradaasociada= models.ForeignKey(Parada,  related_name = "parada_asociada")
-      unidad = models.ForeignKey(Unidad, related_name = "numero_unidad", primary_key=True)
-      tiempo = models.DateTimeField()
-      class Meta:
-         db_table = 'posicionActual'
-         #db_tablespace = 'motorprediccion'
+	latitud = models.FloatField()
+	longitud = models.FloatField()
+	paradaasociada= models.ForeignKey(Parada,  related_name = "parada_asociada")
+	unidad = models.ForeignKey(Unidad, related_name = "numero_unidad", primary_key=True)
+	tiempo = models.DateTimeField()
+
+	class Meta:
+		db_table = 'posicionActual'
           
-      def _unicode_(self): 
-          return self.model_id
+	def _unicode_(self):
+		return self.model_id
 
 class Estimacion():
     tiempo = 0
@@ -119,8 +118,8 @@ class Estimacion():
     unidad = 0
     paradaasociada = 0
     
-    def _unicode_(self): 
-      return self.unidad      
+    def _unicode_(self):
+    	return self.unidad      
     
     def acumular(self, tiempoNew,  desviacionNew):
         self.tiempo = self.tiempo + tiempoNew
@@ -138,15 +137,16 @@ class Estimacion():
         
     
 class TiempoRecorrido(models.Model):
-      promedio = models.FloatField()
-      desstd = models.FloatField()
-      parada= models.ForeignKey(Parada,  related_name = "parada_origen", primary_key=True)
-      orden = models.IntegerField()
-      class Meta:
-         db_table = 'promedios'
-         #db_tablespace = 'motorprediccion'            
-      def _unicode_(self): 
-          return self.promedio
+	promedio = models.FloatField()
+	desstd = models.FloatField()
+	parada= models.ForeignKey(Parada,  related_name = "parada_origen", primary_key=True)
+	orden = models.IntegerField()
+	
+	class Meta:
+		db_table = 'promedios'
+	
+	def _unicode_(self): 
+		return self.promedio
 
 class WorldBorders(models.Model):
         # Regular Django fields corresponding to the attributes in the
@@ -187,80 +187,96 @@ class MyListener(object):
         self.mensaje = message
         
     def getMensaje(self):
-      return self.mensaje
+		return self.mensaje
 
 class Presponse(object):
     colectivo = ''
     tiempo = 0
-     
-    def __init__ (self, c, t):
-        self.colectivo = c
-        self.tiempo = t
-        
+    lat = 0
+    lon = 0
+    
+    def __init__ (self, c, t, la, lo):
+    	self.colectivo = c
+    	self.tiempo = t
+    	self.lat = la
+    	self.lon = lo
+
     def __unicode__(self):
         return self.colectivo
     
 class PresponseHandler(ContentHandler):
- lista = []
- isColeElement= 0;
- isTiempoElement = 0;
- islatElement = 0;
- islonElement = 0;
- colectivo = 0;
- tiempo = 0;
- lat = 0;
- lon = 0; 
+	lista = []
+	isColeElement= 0
+ 	isTiempoElement = 0
+ 	islatElement = 0
+ 	islonElement = 0
+ 	isTimeStampElement = 0
+ 	colectivo = ""
+ 	tiempo = ""
+ 	lat = ""
+ 	lon = "" 
+ 	timestamp = ""
  
- def __init__ (self):
-   self.lista = []
-   self.isColeElement= 0;
-   self.isTiempoElement = 0;
-   self.islatElement = 0;
-   self.islonElement = 0;
-   self.colectivo = 0;
-   self.tiempo = 0;
-   self.lat = 0;
-   self.lon = 0;
-   
- def startElement(self, name, attrs):
-   if name == 'busId':     
-     self.isColeElement= 1;
-     self.colectivo = "";
-   elif name == 'timeSec':
-     self.isTiempoElement = 1;
-     self.tiempo = "";
-   elif name == 'lat':
-     self.islatElement = 1;
-     self.lat = "";
-   elif name == 'lon':
-     self.islonElement = 1;
-     self.long = "";   
-   return
-
- def characters (self, ch):
-   if self.isColeElement== 1:
-     self.colectivo += ch
-   if self.isTiempoElement == 1:
-     self.tiempo += ch
-   if self.islatElement == 1:
-     self.lat += ch
-   if self.islonElement == 1:
-     self.lon += ch
-
-
- def endElement(self, name):
-   if name == 'busId':
-     self.isColeElement= 0
-   if name == 'timeSec':
-     self.inTiempoContent = 0
-   if name == 'lat':
-     self.inlatContent = 0
-   if name == 'lon':
-     self.inlonContent = 0
-   if name == 'prediction':
-       self.lista = self.lista + [Presponse(self.colectivo, self.tiempo)]
-   if name == 'prediction-responde':
-       print self.lista
-   
- def obtenerLista(self):
-   return self.lista
+ 	def __init__ (self):
+ 		self.lista = []
+ 		self.isColeElement= 0
+ 		self.isTiempoElement = 0
+ 		self.islatElement = 0
+ 		self.islonElement = 0
+ 		self.isTimeStampElement = 0
+ 		self.colectivo = ""
+ 		self.tiempo = ""
+ 		self.lat = ""
+ 		self.lon = ""
+ 		self.timestamp = ""
+ 		
+	def startElement(self, name, attrs):
+		if name == 'busId':
+			self.isColeElement= 1
+			self.colectivo = ""
+	 	elif name == 'timeSec':
+	 		self.isTiempoElement = 1
+	 		self.tiempo = ""
+ 		elif name == 'lat':
+ 			self.islatElement = 1
+ 			self.lat = ""
+ 		elif name == 'lon':
+ 			self.islonElement = 1
+ 			self.lon = ""
+		elif name == 'timestamp':
+			self.isTimeStampElement = 1
+			self.timestamp = ""
+	
+	def characters (self, ch):
+		if self.isColeElement== 1:
+			self.colectivo += ch
+		if self.isTiempoElement == 1:
+			self.tiempo += ch
+		if self.islatElement == 1:
+			self.lat = ch
+		if self.islonElement == 1:
+			self.lon = ch
+		if self.isTimeStampElement == 1:
+			self.timestamp += ch
+	
+	def endElement(self, name):
+		if name == 'busId':
+			self.isColeElement= 0
+		if name == 'timeSec':
+			self.inTiempoContent = 0
+		if name == 'lat':
+			self.inlatContent = 0
+		if name == 'lon':
+			self.inlonContent = 0
+		if name == 'prediction':
+			self.lista = self.lista + [Presponse(self.colectivo, self.tiempo,self.lat, self.lon)]
+		if name == 'timestamp':
+			self.isTimeStampElement = 0
+		if name == 'prediction-responde':
+			print self.lista
+	
+	def obtenerLista(self):
+		return self.lista
+	
+	def obtenerTimeStamp(self):
+		return self.timestamp
