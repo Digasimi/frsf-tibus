62a38f984933ad61fe07807d026c3932cae87a85
digasimi@gmail.com <digasimi@gmail.com@1eb605dd-4156-0cf0-e7f9-35fc125b5a16>
2012-08-22 21:35:41 +0000

    webTibus: Pagina de inicio de sesion adaptada a la estetica del sitio Separacion de aplicacion de administracion y consulta

---
+++ webTibus/tibusAdmin/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+
+#Formulario con los datos para cargar una parada
+class FormularioParada(forms.Form):
+    linea = forms.CharField(widget=forms.HiddenInput)
+    orden = forms.IntegerField(required=False)
+    latitud = forms.FloatField()
+    longitud = forms.FloatField()
+    calle1 = forms.CharField()
+    calle2 = forms.CharField()
+    paradaActiva = forms.BooleanField(required=False)
+    
+    def aumentarOrden(self):
+        self.orden = self.orden +1
+
+#Formulario con los datos para cargar una linea
+class FormularioRecorrido(forms.Form):
+    linea = forms.CharField()
+    frecuencia = forms.IntegerField()
+    empresa = forms.CharField(widget=forms.HiddenInput)
+    masivo = forms.FileField(required=False)
+    
+#Formulario con los datos para cargar una unidad de colectivo
+class FormularioUnidad(forms.Form):
+    linea = forms.CharField(widget=forms.HiddenInput)
+    aptoMovilidadReducida = forms.BooleanField(required=False)
+    id_unidad_linea = forms.IntegerField()
+
+class FormularioEmpresa(forms.Form):
+    nombre = forms.CharField()
+    email = forms.EmailField()
+    
+class FormularioUsuario(forms.Form):
+    nombre = forms.CharField()
+    email = forms.EmailField()
+    categoria = forms.CharField(widget=forms.HiddenInput)
+    empresa = forms.CharField(required=False, widget=forms.HiddenInput)
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirmacion = forms.CharField(widget=forms.PasswordInput)
