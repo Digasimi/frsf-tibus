a40d8cb84197f813deba6e3442cee56530f980c0
danigzz <danigzz@1eb605dd-4156-0cf0-e7f9-35fc125b5a16>
2012-09-06 23:16:20 +0000

    Las paradas se traen en orden para los recorridos. Nuevos metodos de busqueda de paradas

--- trunk/tibusServer/src/main/java/frsf/tibus/prediction/model/averagespeed/Route.java
+++ trunk/tibusServer/src/main/java/frsf/tibus/prediction/model/averagespeed/Route.java
@@ -1,9 +1,13 @@
 package frsf.tibus.prediction.model.averagespeed;
 
 import java.util.ArrayList;
+
 import java.util.List;
 
 import javax.persistence.*;
+
+import com.bbn.openmap.proj.Length;
+import com.bbn.openmap.proj.coords.LatLonPoint;
 
 import frsf.tibus.domain.PredictionResponse;
 import frsf.tibus.domain.PredictionResponse.Prediction;
@@ -15,7 +19,9 @@ public class Route {
 	@Column(name="idrecorrido")
 	private Integer routeId;
 	
-	@OneToMany(targetEntity=Stop.class, mappedBy = "route", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@OneToMany(targetEntity=Stop.class, mappedBy = "route", 
+			fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@OrderBy("orden asc")
 	private List<Stop> stops;
 	
 	@Transient
@@ -59,18 +65,24 @@ public class Route {
 
 
 	public PredictionResponse getPredictions(String stopId) {
+		
 		PredictionResponse result = new PredictionResponse();
-		Stop destination = this.getStop(stopId);
+		Stop destination = this.getStopById(stopId);
+		
 		if (destination != null)
 		{
 			for(Bus bus: buses)
 			{
 				if (bus.getCurrentStop().getOrder() <= destination.getOrder())
 				{
-					Float time = new Float(0);
+					Double time = new Double(0);
+					Stop tempStop1, tempStop2;
 					for(int i = bus.getCurrentStop().getOrder(); i < destination.getOrder();i++){
-						time += this.getStopByOrder(i).getDistance(this.getStopByOrder(i+1))/
-								this.getStopByOrder(i).getAverageSpeed().getAverageSpeed();
+						tempStop1 = getStopByOrder(i);
+						tempStop2 = getNextStop(tempStop1);
+						
+						time += (getDistance(tempStop1, tempStop2)/
+								tempStop1.getAverageSpeed().getAverageSpeed());						
 					}
 					result.addPrediction(new Prediction(bus.getId().toString(),new Integer(time.intValue()), bus.getLat(), bus.getLon()));
 				}
@@ -87,8 +99,8 @@ public class Route {
 		return result;
 	}
 
-	private Stop getStop(String parada){
-		Integer d = new Integer(parada);
+	public Stop getStopById(String stopId){
+		Integer d = new Integer(stopId);
 		for(Stop stop:stops){
 			if (stop.getStopId().equals(d)){
 				return stop;
@@ -97,14 +109,77 @@ public class Route {
 		return null;
 	}
 	
-	private Stop getStopByOrder(Integer orden){
-		for(Stop stop:stops){
-			if (stop.getOrder().equals(orden)){
-				return stop;
-			}
+	public Stop getStopByOrder(Integer order){
+		if(order <= this.stops.size())
+			return stops.get(order-1);
+		else
+			return null;
+	}
+	
+	
+	/**
+	 * Calcula la distancia entre dos paradas.
+	 * El cálculo se hace en el sentido del recorrido.
+	 * @param start: Parada origen
+	 * @param destination: Parada destino
+	 * @return
+	 */
+	public Double getDistance(Stop start, Stop destination)
+	{	
+		if(start.equals(destination))
+			return new Double(0);
+		
+		// Si las paradas son consecutivas
+		if(consecutive(start,destination))
+		{
+			Double distance = Length.METER.fromRadians(new LatLonPoint.Float(start.getLat(),start.getLon())
+			.distance(new LatLonPoint.Float(destination.getLat(),destination.getLon())));
+			return distance;
 		}
-		return null;
+		else
+		{
+			Stop nextStop = getNextStop(start);
+			return getDistance(start, nextStop) + 
+					getDistance(nextStop, destination);
+		}	
+	}
+	
+	
+	/**
+	 * 
+	 * @param start
+	 * @return
+	 */
+	public Stop getNextStop(Stop start) {
+		if(start.getOrder() < stops.size())
+			return stops.get(start.getOrder()+1);
+		else
+			return stops.get(0);		
 	}
 
+	/**
+	 * Determina si dos paradas son consecutivas. Se calcula en el sentido de circulación del 
+	 * recorrido.
+	 * Tener en cuenta que el recorrido es continuo, por lo que la ultima y primera parada 
+	 * son consecutivas si se evaluan en ese orden.
+	 * @param start: Parada de origen
+	 * @param destination: Parada de destino
+	 * @return devuelve true si son consecutivas, false de lo contrario
+	 */
 
+	public boolean consecutive(Stop start, Stop destination) {
+		Integer startOrder = start.getOrder();
+		Integer destinationOrder = destination.getOrder();
+		
+		if(startOrder <= destinationOrder)
+			if(startOrder == destinationOrder+1)
+				return true;
+			else
+				return false;
+		else
+			if(startOrder == stops.size() && destinationOrder == 1)
+				return true;
+			else
+				return false;
+	}
 }
