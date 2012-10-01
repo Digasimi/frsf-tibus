c7df5010819892a63b1b04eb6504405ce968a192
danigzz <danigzz@1eb605dd-4156-0cf0-e7f9-35fc125b5a16>
2012-10-01 21:24:02 +0000

    Nuevo estilo de formularios

--- webTibus/tibus/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class PredictionForm(forms.Form):
-    linea = forms.CharField(widget=forms.HiddenInput)
-    orden = forms.IntegerField(widget=forms.HiddenInput)
-    apto = forms.BooleanField(required=False)
