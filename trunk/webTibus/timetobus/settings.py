a0462e40697efa0864bbc4563c532d7381e6a673
digasimi@gmail.com <digasimi@gmail.com@1eb605dd-4156-0cf0-e7f9-35fc125b5a16>
2012-10-10 21:21:50 +0000

    Implementado crispy en form de prediccion

--- trunk/webTibus/timetobus/settings.py
+++ trunk/webTibus/timetobus/settings.py
@@ -132,6 +132,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'django.contrib.gis',
+    'crispy_forms',
     'tibusAdmin', 
     'tibus',
 )
