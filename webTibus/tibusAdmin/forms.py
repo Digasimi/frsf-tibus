6372cd39cfb43c8bd6f75cc913da4e11e7331cb9
digasimi@gmail.com <digasimi@gmail.com@1eb605dd-4156-0cf0-e7f9-35fc125b5a16>
2012-10-17 22:57:26 +0000

    Unico template para tablas de datos/Carga de datos Implementados form generico fixed Bugs

--- webTibus/tibusAdmin/forms.py
+++ webTibus/tibusAdmin/forms.py
@@ -5,6 +5,31 @@ from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit
 from crispy_forms.bootstrap import FormActions
 
 #Formulario con los datos para cargar una parada
+class StopsForm(forms.Form):
+    orden = forms.IntegerField(required=False, label='Orden de la parada seleccionada')
+    masivo = forms.FileField(required=False, help_text = "Las paradas deben estan en orden y una por linea con el formato: Latitud; Longitud;Calle;Interseccion",label='Archivo con lista de paradas')
+    action = forms.CharField(widget=forms.HiddenInput)
+    
+    def __init__(self, *args, **kwargs):
+        self.helper = FormHelper()
+        self.helper.form_id = 'id-StopListForm'
+        self.helper.form_method = 'post'
+        self.helper.layout = Layout(
+            Fieldset(
+                'Lista de paradas',
+                'orden',
+                'masivo',
+                'action',                            
+            ),
+            FormActions(
+                Submit('action', 'Carga Masiva', css_class="btn-primary"),
+                Submit('action', 'Agregar', css_class="btn-primary"),
+                Submit('action', 'Modificar', css_class="btn-primary"),
+                Submit('action', 'Eliminar', css_class="btn-primary")
+            ),
+        )
+        super(StopsForm, self).__init__(*args, **kwargs)
+        
 class StopForm(forms.Form):
     linea = forms.ModelChoiceField(queryset=Recorrido.objects.all(), empty_label=None, widget=forms.HiddenInput)
     orden = forms.ModelChoiceField(queryset=Parada.objects.all(), required=False, empty_label='Parada Inicial')
@@ -21,10 +46,7 @@ class StopForm(forms.Form):
     def __init__(self, *args, **kwargs):
         self.helper = FormHelper()
         self.helper.form_id = 'id-StopForm'
-        #self.helper.form_class = 'blueForms'
         self.helper.form_method = 'post'
-        self.helper.form_action = 'submit_survey'
-        
         self.helper.layout = Layout(
             Fieldset(
                 'Datos de la parada',
@@ -40,35 +62,27 @@ class StopForm(forms.Form):
                 Submit('save', 'Guardar', css_class='button white')
             )
         )
-
-        #self.helper.add_input(Submit('save', 'Guardar'))
         super(StopForm, self).__init__(*args, **kwargs)
 
 #Formulario con los datos para cargar una route
 class RoutesForm(forms.Form):
-    linea = forms.ModelChoiceField(queryset=Recorrido.objects.all(), empty_label=None)
-    empresa = forms.ModelChoiceField(queryset=Empresa.objects.all(), empty_label=None)
+    identificador = forms.CharField(label='Nombre/identificador de la linea',initial='0', widget=forms.HiddenInput)
     
     def __init__(self, *args, **kwargs):
         self.helper = FormHelper()
         self.helper.form_id = 'id-RoutesForm'
-        #self.helper.form_class = 'blueForms'
         self.helper.form_method = 'post'
-        self.helper.form_action = ''
-        
         self.helper.layout = Layout(
             Fieldset(
-                'Datos de la parada',
-                'linea',
-                'empresa',                                
+                'Acciones',
+                'identificador',                                
             ),
             FormActions(
-                Submit('action', 'addRoute', css_class="btn-primary"),
-                Submit('action','editRoute', css_class="btn-primary"),
-                Submit('action','delRoute')
+                Submit('action','Agregar', css_class="btn-primary"),
+                Submit('action','Modificar', css_class="btn-primary"),
+                Submit('action','Eliminar', css_class="btn-primary")
             ),
         )
-        #self.helper.add_input(Submit('save', 'Guardar'))
         super(RoutesForm, self).__init__(*args, **kwargs)
 
     
@@ -76,21 +90,50 @@ class RouteForm(forms.Form):
     linea = forms.CharField(label='Nombre/identificador de la linea')
     frecuencia = forms.IntegerField(required=False,label='Frecuencia de salida')
     empresa = forms.ModelChoiceField(queryset=Empresa.objects.all(), empty_label=None,label='Empresa asociada')
-    masivo = forms.FileField(required=False, help_text = "Las paradas deben estan en orden y una por linea con el formato: Latitud; Longitud;Calle;Interseccion",label='Archivo con lista de paradas')
     action = forms.CharField(widget=forms.HiddenInput)
     
     def __init__(self, *args, **kwargs):
         self.helper = FormHelper()
         self.helper.form_id = 'id-RouteForm'
         self.helper.form_method = 'post'
-        self.helper.form_action = ''
-
-        self.helper.add_input(Submit('save', 'Guardar'))
-        self.helper.add_input(Submit('stops', 'Paradas'))
-        self.helper.add_input(Submit('frecuency', 'Frecuencias'))
+        self.helper.layout = Layout(
+            Fieldset(
+                'Datos de la linea',
+                'linea',
+                'frecuencia',
+                'empresa',
+                'action',
+            ),
+            ButtonHolder(
+                Submit('save', 'Guardar', css_class='button white'),
+                Submit('stops', 'Paradas', css_class='button white'),
+                Submit('frecuency', 'Frecuencias', css_class='button white')
+            )
+        )
         super(RouteForm, self).__init__(*args, **kwargs)
     
 #Formulario con los datos para cargar una unidad de colectivo
+class BussForm(forms.Form):
+    identificador = forms.CharField(label='Nombre/identificador de la unidad',initial='0', widget=forms.HiddenInput)
+    
+    def __init__(self, *args, **kwargs):
+        self.helper = FormHelper()
+        self.helper.form_id = 'id-BussForm'
+        self.helper.form_method = 'post'
+        self.helper.layout = Layout(
+            Fieldset(
+                'Acciones',
+                'identificador',                                
+            ),
+            FormActions(
+                Submit('action','Agregar', css_class="btn-primary"),
+                Submit('action','Modificar', css_class="btn-primary"),
+                Submit('action','Eliminar', css_class="btn-primary")
+            ),
+        )
+        super(BussForm, self).__init__(*args, **kwargs)
+        
+
 class BusForm(forms.Form):
     linea = forms.ModelChoiceField(queryset=Recorrido.objects.all(), empty_label=None, label='Linea la que pertenece la unidad')
     apto_movilidad_reducida = forms.BooleanField(required=False, label ='Unidad con rampa')
@@ -100,10 +143,7 @@ class BusForm(forms.Form):
     def __init__(self, *args, **kwargs):
         self.helper = FormHelper()
         self.helper.form_id = 'id-BusForm'
-        #self.helper.form_class = 'blueForms'
         self.helper.form_method = 'post'
-        self.helper.form_action = 'submit_survey'
-        
         self.helper.layout = Layout(
             Fieldset(
                 'Unidad',
@@ -115,9 +155,28 @@ class BusForm(forms.Form):
                 Submit('save', 'Guardar', css_class="btn-primary")                
             ),
         )
-
-        #self.helper.add_input(Submit('save', 'Guardar'))
         super(BusForm, self).__init__(*args, **kwargs)
+
+class CompaniesForm(forms.Form):
+    identificador = forms.CharField(label='Nombre/identificador de la empresa',initial='', widget=forms.HiddenInput)
+    
+    def __init__(self, *args, **kwargs):
+        self.helper = FormHelper()
+        self.helper.form_id = 'id-CompaniesForm'
+        self.helper.form_method = 'post'
+        self.helper.layout = Layout(
+            Fieldset(
+                'Acciones',
+                'identificador',                                
+            ),
+            FormActions(
+                Submit('action','Agregar', css_class="btn-primary"),
+                Submit('action','Modificar', css_class="btn-primary"),
+                Submit('action','Eliminar', css_class="btn-primary")
+            ),
+        )
+        super(CompaniesForm, self).__init__(*args, **kwargs)
+        
 
 class CompanyForm(forms.Form):
     nombre = forms.CharField(label='Nombre de la empresa')
@@ -127,13 +186,40 @@ class CompanyForm(forms.Form):
     def __init__(self, *args, **kwargs):
         self.helper = FormHelper()
         self.helper.form_id = 'id-CompanyForm'
-        self.helper.form_class = 'blueForms'
         self.helper.form_method = 'post'
-        self.helper.form_action = 'submit_survey'
-
-        self.helper.add_input(Submit('save', 'Guardar'))
+        self.helper.layout = Layout(
+            Fieldset(
+                'Datos de la empresa',
+                'nombre',
+                'email',
+                'action',                            
+            ),
+            FormActions(
+                Submit('save', 'Guardar', css_class="btn-primary")                
+            ),
+        )
         super(CompanyForm, self).__init__(*args, **kwargs)
+
+class UsersForm(forms.Form):
+    identificador = forms.CharField(label='Nombre del Usuario',initial='', widget=forms.HiddenInput)
     
+    def __init__(self, *args, **kwargs):
+        self.helper = FormHelper()
+        self.helper.form_id = 'id-UsersForm'
+        self.helper.form_method = 'post'
+        self.helper.layout = Layout(
+            Fieldset(
+                'Acciones',
+                'identificador',                               
+            ),
+            FormActions(
+                Submit('action','Agregar', css_class="btn-primary"),
+                Submit('action','Modificar', css_class="btn-primary"),
+                Submit('action','Eliminar', css_class="btn-primary")
+            ),
+        )
+        super(UsersForm, self).__init__(*args, **kwargs)
+            
 class UserForm(forms.Form):
     nombre = forms.CharField(label='Nombre')
     email = forms.EmailField(required=False,label='email')
@@ -146,10 +232,7 @@ class UserForm(forms.Form):
     def __init__(self, *args, **kwargs):
         self.helper = FormHelper()
         self.helper.form_id = 'id-UserForm'
-        #self.helper.form_class = 'blueForms'
         self.helper.form_method = 'post'
-        self.helper.form_action = 'submit_survey'
-        
         self.helper.layout = Layout(
             Fieldset(
                 'Datos de usuario',
@@ -165,8 +248,6 @@ class UserForm(forms.Form):
                 Submit('save', 'Guardar', css_class="btn-primary")                
             ),
         )
-
-        #self.helper.add_input(Submit('save', 'Guardar'))
         super(UserForm, self).__init__(*args, **kwargs)
     
 class PasswordForm(forms.Form):
@@ -178,27 +259,17 @@ class PasswordForm(forms.Form):
     def __init__(self, *args, **kwargs):
         self.helper = FormHelper()
         self.helper.form_id = 'id-PasswordForm'
-        self.helper.form_class = 'blueForms'
         self.helper.form_method = 'post'
-        self.helper.form_action = 'submit_survey'
-
-        self.helper.add_input(Submit('save', 'Guardar'))
+        self.helper.layout = Layout(
+            Fieldset(
+                'Cambio de password',
+                'nombre',
+                'oldPassword',
+                'newPassword',
+                'confirmacion',
+            ),
+            FormActions(
+                Submit('save', 'Guardar', css_class="btn-primary")                
+            ),
+        )
         super(PasswordForm, self).__init__(*args, **kwargs)
-
-class StopsForm(forms.Form):
-    orden = forms.IntegerField(required=False, label='Orden de la parada seleccionada')
-    masivo = forms.FileField(required=False, help_text = "Las paradas deben estan en orden y una por linea con el formato: Latitud; Longitud;Calle;Interseccion",label='Archivo con lista de paradas')
-    action = forms.CharField(widget=forms.HiddenInput)
-    
-    def __init__(self, *args, **kwargs):
-        self.helper = FormHelper()
-        self.helper.form_id = 'id-StopListForm'
-        self.helper.form_class = 'blueForms'
-        self.helper.form_method = 'post'
-        self.helper.form_action = 'submit_survey'
-
-        self.helper.add_input(Submit('action', 'addMasiveStop'))
-        self.helper.add_input(Submit('action', 'addStop'))
-        self.helper.add_input(Submit('action', 'editStop'))
-        self.helper.add_input(Submit('action', 'delStop'))
-        super(StopsForm, self).__init__(*args, **kwargs)
