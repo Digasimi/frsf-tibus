53792560ff31145557381d556eeaf3e3b26c6108
digasimi@gmail.com <digasimi@gmail.com@1eb605dd-4156-0cf0-e7f9-35fc125b5a16>
2012-11-24 00:29:09 +0000

    Implementada estimacion de tiempo de viaje Agregado logo al sistema (aun falta actualizar logo)

--- webTibus/tibus/forms.py
+++ webTibus/tibus/forms.py
@@ -49,3 +49,32 @@ class ItineraryForm(forms.Form):
         
     def quitEmptyOption(self):
         self.fields['linea'].empty_label = None
+        
+class TravelForm(forms.Form):
+    linea = forms.ModelChoiceField(queryset=Recorrido.objects.filter(predictable = True), empty_label='Seleccione una linea', label='Linea')
+    linea.widget.attrs["onchange"]="this.form.submit()"
+    origen = forms.ModelChoiceField(queryset=Parada.objects.none(), empty_label='Debe Seleccionar la linea primero', label='Parada origen', required=False)
+    destino = forms.ModelChoiceField(queryset=Parada.objects.none(), empty_label='Debe Seleccionar la linea primero', label='Parada destino', required=False)
+    
+    def __init__(self, *args, **kwargs):
+        self.helper = FormHelper()
+        self.helper.form_id = 'id-travelForm'
+        self.helper.form_method = 'post'
+        
+        self.helper.layout = Layout(
+            Fieldset(
+                'Tiempos de viaje',
+                'linea',
+                'origen',
+                'destino',
+            ),
+        )
+        super(TravelForm, self).__init__(*args, **kwargs)
+        
+    def setQueryOrden(self, lineaId):
+        self.fields['origen'].queryset = Parada.objects.filter(linea = lineaId)
+        self.fields['origen'].empty_label = None
+        self.fields['destino'].queryset = Parada.objects.filter(linea = lineaId)
+        self.fields['destino'].empty_label = None
+        self._errors = {}
+        self.helper.add_input(Submit('action', 'resultado'))
--- webTibus/tibus/views.py
+++ webTibus/tibus/views.py
@@ -6,9 +6,9 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.db.utils import DatabaseError
 from django.http import HttpResponseRedirect
-from tibus.forms import PredictionForm, ItineraryForm
+from tibus.forms import PredictionForm, ItineraryForm, TravelForm
 from tibus.models import Parada, Recorrido, Unidad, Frecuencia
-from tibus.listenerParseXML import MyListener, PresponseHandler
+from tibus.listenerParseXML import MyListener, PresponseHandler, Presponse
 from xml.sax import parseString,  SAXParseException
 
 def index(request): #pagina principal
@@ -63,7 +63,7 @@ def tibushelp(request):#pagina de ayuda
 def createMessage(route,  order):
     return '<prediction-request><linea>' + str(route) + '</linea><parada>' + str(order) + '</parada></prediction-request>'
 
-def result(request): #pagina que mostrara las predicciones
+def arriveResult(request): #pagina que mostrara las predicciones
     #carga inicial
     c = {}
     c.update(csrf(request))
@@ -162,3 +162,139 @@ def itinerary(request):
         frecuencyList = []
     
     return render_to_response('itinerario.html',  {'stopList':stopList, 'frecuencyList':frecuencyList, 'error': errorDescription, 'form': form},  context_instance=RequestContext(request))
+
+def travelPrediction(request): #pagina que mostrara las predicciones
+    #carga inicial
+    c = {}
+    c.update(csrf(request))
+    errorDescription = ""
+        
+    #logica
+    if request.method == 'POST':
+        form = TravelForm(request.POST)
+        try:
+            if request.POST.get('action')=='resultado':
+                routePrediction = Recorrido.objects.get(idrecorrido=request.POST.get('linea'))
+                origenStopPrediction = Parada.objects.get(idparada = request.POST.get('origen'))
+                destinyStopPrediction = Parada.objects.get(idparada = request.POST.get('destino'))
+                if (destinyStopPrediction.getOrder() > origenStopPrediction.getOrder()):
+                    return HttpResponseRedirect('rViaje?linea=' + str(routePrediction.getId()) + '&origen='+ str(origenStopPrediction.getId())+'&destino='+ str(destinyStopPrediction.getId()))
+                else:
+                    errorDescription = "La parada destino debe ser posterior a la parada origen"
+                    form.setQueryOrden(request.POST.get('linea'))
+            else:
+                form.setQueryOrden(request.POST.get('linea'))
+            #empiezan las excepciones
+        except ValueError:
+            errorDescription = "Datos en formato incorrecto - Valor de datos"
+        except DatabaseError:
+            errorDescription = "Error de no se que" #Ver cuando salta este error. posible error de asociacion route-parada
+        except stomp.exception.ConnectFailedException:
+            errorDescription = "No hay conexion con el servidor"
+        except Recorrido.DoesNotExist:
+            errorDescription = "No existe la route"
+        except Parada.DoesNotExist:
+            errorDescription = "No existe la parada"
+    else:
+        form = TravelForm()
+    return render_to_response('prediccion.html',  {'form':form, 'error': errorDescription,  'admin': False},  context_instance=RequestContext(request))
+
+def travelResult(request): #pagina que mostrara las predicciones
+    #carga inicial
+    c = {}
+    c.update(csrf(request))
+    predictionList = []
+    errorDescription = ""
+    timeStampPrediction = ""
+    parser = PresponseHandler()
+    stopList = Parada.objects.all()
+    
+    #logica
+    routeName = request.GET.get('linea')
+    origenStopId= request.GET.get('origen')
+    destinyStopId= request.GET.get('destino')
+    if request.POST.get('action') == 'prediction':
+        return HttpResponseRedirect('viaje')
+    try:
+        if routeName == '':
+            errorDescription = "No ingreso la linea"
+        elif destinyStopId == '' or origenStopId == '':
+            errorDescription = "No ingreso la parada"
+        else:
+            temporaryRoute= Recorrido.objects.get(idrecorrido = routeName)
+            stopList = Parada.objects.filter(linea = temporaryRoute).order_by('orden')
+            origenStop = Parada.objects.get(idparada = origenStopId)
+            destinyStop = Parada.objects.get(idparada = destinyStopId)
+            
+            #formato nuevo
+            conn = stomp.Connection([('127.0.0.1',61613)]) #Aca hay que definir el conector externo
+            conn.start()
+            conn.connect()
+            responseQueue = '/temp-queue/responseQueue'
+            conn.set_listener('list', MyListener())
+            #Estimacion origen
+            msg = createMessage(temporaryRoute.getLinea(),  origenStop.getId())
+            conn.send(msg, destination='/queue/predictions.requests',headers={'reply-to':responseQueue})
+            conn.subscribe(destination=responseQueue, ack='auto')
+            predictionXml = ""
+            lis1 = conn.get_listener('list')
+            timer = 0
+            while (predictionXml == '' and timer <= 15):
+                time.sleep(1)
+                timer=timer+1
+                predictionXml = lis1.getMessage()
+            if (timer == 15):
+                errorDescription = 'Tiempo de espera agotado'
+            else:
+                parseString(predictionXml, parser)
+                predictionList1 = parser.getLista()
+                errorDescription = parser.getError()
+                timeStampPrediction = parser.getTimeStamp()
+            if errorDescription == "":
+                #Estimacion destino
+                msg = createMessage(temporaryRoute.getLinea(),  destinyStop.getId())
+                conn.send(msg, destination='/queue/predictions.requests',headers={'reply-to':responseQueue})
+                conn.subscribe(destination=responseQueue, ack='auto')
+                predictionXml = ""
+                lis1 = conn.get_listener('list')
+                timer = 0
+                while (predictionXml == '' and timer <= 15):
+                    time.sleep(1)
+                    timer=timer+1
+                    predictionXml = lis1.getMessage()
+                if (timer == 15):
+                    errorDescription = 'Tiempo de espera agotado'
+                else:
+                    parseString(predictionXml, parser)
+                    predictionList2 = parser.getLista()
+                    errorDescription = parser.getError()
+                #Falta calcular tiempos de viaje
+                if errorDescription == "":
+                    if predictionList1 == [] or predictionList2 == []:
+                        errorDescription = "No hay predicciones disponibles"
+                    else:
+                        for prediccion1 in predictionList1:
+                            for prediccion2 in predictionList2:
+                                if prediccion1.bus == prediccion2.bus:
+                                    predictionList = [Presponse(prediccion1.bus, (prediccion2.time - prediccion1.time))]
+                        if predictionList == []:
+                            errorDescription = "No hay predicciones disponibles"
+            conn.unsubscribe(destination=responseQueue)
+            conn.disconnect()
+        #empiezan las excepciones
+    except SAXParseException:
+        errorDescription = "Datos en formato incorrecto - Error de conexion con servidor"
+        #Ver posibilidad de no estimacion
+    except ValueError:
+        errorDescription = "Datos en formato incorrecto - Valor de datos"
+    except DatabaseError:
+        errorDescription = "Error de no se que" #Ver cuando salta este error. posible error de asociacion route-parada
+    except stomp.exception.ConnectFailedException:
+        errorDescription = "No hay conexion con el servidor"
+    except Recorrido.DoesNotExist:
+        errorDescription = "No existe la route"
+    except Parada.DoesNotExist:
+        errorDescription = "No existe la parada"
+    except Unidad.DoesNotExist:
+        errorDescription = "No hay unidades existentes"
+    return render_to_response('vResultado.html',  {'route': temporaryRoute, 'stopList': stopList, 'predicciones':predictionList,  'error': errorDescription,  'admin': False,  'timeStamp': timeStampPrediction, 'linea': temporaryRoute.getLinea(), 'origen': origenStop,'destino': destinyStop},  context_instance=RequestContext(request))
